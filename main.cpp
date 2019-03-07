87610608ae2522a3d2c50799226866a9c2d949cd
TylerHinojosa <46981261+TylerHinojosa@users.noreply.github.com>
2019-03-06 18:10:46 -0600

    Update main.cpp

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int findQuarters(int x){
+	return (x/25);
+}
+
+int findDimes(int x){
+	return (x/10);
+}
+
+int findNickles(int x){
+	return (x/5);
+}
+
+int main(){
+	int change = 0;
+	
+	cout << "Enter the amount of change you want: ";
+	cin >> change;
+	
+	cout << "You would have " << findQuarters(change) << " quarters(s)" << endl;
+	cout << "You would have " << findDimes((change % 25)) << " dime(s)" << endl;
+	cout << "You would have " << findNickles(((change % 25) % 10)) << " nickel(s)" << endl;
+	cout << "and " << (((change % 25) % 10) % 5) << " penny(s)" << endl;
+	
+}
